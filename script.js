fb52112088b7742325da9a897d2f88610584dbbe
MathisSTL <141674101+MathisSTL@users.noreply.github.com>
2023-08-26 16:10:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const correctAnswers = {
+  q1: "d",
+  q2: "b",
+  q3: "c",
+  q4: "d",
+  q5: "d",
+  q6: "b",
+  q7: "b",
+  q8: "a"
+};
+
+const submitButton = document.getElementById("submit-btn");
+const resultMessage = document.getElementById("result");
+const chiffreElement = document.getElementById("chiffre");
+
+submitButton.addEventListener("click", () => {
+  const form = document.getElementById("quiz-form");
+  const formData = new FormData(form);
+  let correctCount = 0;
+
+  for (const [question, answer] of formData) {
+    if (correctAnswers[question] === answer) {
+      correctCount++;
+    }
+  }
+
+  if (correctCount === Object.keys(correctAnswers).length) {
+    resultMessage.style.display = "block";
+    chiffreElement.textContent = "0";
+  }
+});
